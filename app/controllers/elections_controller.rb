8e9308aa6828d22fea11b8ff98888990ec31a227
Jeff Cook <jeff.the.cook@gmail.com>
2012-03-13 22:28:23 -0700

    Added the ability to select the default election

--- app/controllers/elections_controller.rb
+++ app/controllers/elections_controller.rb
@@ -1,43 +1,40 @@
 class ElectionsController < ApplicationController
   # GET /elections
-  # GET /elections.json
   def index
     @elections = Election.all
+    if (params[:select])
+      @election = Election.find(params[:id])
+      ename = @election.name+" ("+@election.day.to_s+")"
+      if (Selection.all.length < 1)
+        se = Selection.new(:eid => @election.id, :ename => ename)
+      else
+        se = Selection.all[0]
+        se.eid = @election.id
+        se.ename = ename
+      end
+      se.save
+    end
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @elections }
     end
   end
 
   # GET /elections/1
-  # GET /elections/1.json
   def show
     @election = Election.find(params[:id])
-    ename = @election.name+" ("+@election.day.to_s+")"
-    if (Selection.all.length < 1)
-      se = Selection.new(:eid => @election.id, :ename => ename)
-    else
-      se = Selection.all[0]
-      se.eid = @election.id
-      se.ename = ename
-    end
-    se.save
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @election }
     end
   end
 
   # GET /elections/new
-  # GET /elections/new.json
   def new
     @election = Election.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @election }
     end
   end
 
@@ -47,46 +44,38 @@ class ElectionsController < ApplicationController
   end
 
   # POST /elections
-  # POST /elections.json
   def create
     @election = Election.new(params[:election])
 
     respond_to do |format|
       if @election.save
         format.html { redirect_to @election, notice: 'Election was successfully created.' }
-        format.json { render json: @election, status: :created, location: @election }
       else
         format.html { render action: "new" }
-        format.json { render json: @election.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PUT /elections/1
-  # PUT /elections/1.json
   def update
     @election = Election.find(params[:id])
 
     respond_to do |format|
       if @election.update_attributes(params[:election])
         format.html { redirect_to @election, notice: 'Election was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @election.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /elections/1
-  # DELETE /elections/1.json
   def destroy
     @election = Election.find(params[:id])
     @election.destroy
 
     respond_to do |format|
       format.html { redirect_to elections_url }
-      format.json { head :no_content }
     end
   end
 
